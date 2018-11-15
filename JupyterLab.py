595988c8ad3831b5ea9561d5018194fe32cad630
Huayong Kuang <sudohuoty@163.com>
2018-11-16 01:09:51 +0800

    基本功能完成

---
+++ JupyterLab.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Huoty, All rights reserved
+# Author: Huoty <sudohuoty@gmail.com>
+# CreateTime: 2018-11-15 22:56:23
+
+import os
+import sys
+import json
+import subprocess
+import webview
+import jupyterlab
+
+
+class ViewApp(object):
+
+    def __init__(self):
+        self.cwd = os.path.abspath(os.path.dirname(__file__))
+        self.lab_cwd = os.path.join(self.cwd, "notebooks")
+        self.lab_proc = None
+        self.lab_version = jupyterlab.__version__
+
+        os.chdir(self.cwd)
+
+        try:
+            with open("config.json") as fp:
+                self.config = json.load(fp)
+        except IOError:
+            self.config = {}
+
+        self.lab_python_path = self.config.get("pythonPath", sys.executable)
+        self.lab_port = self.config.get("labPort", "12580")
+        self.lab_log_path = self.config.get("labPath", "lab.log")
+        self.notebook_config_path = self.config.get(
+            "notebookConfigPath",
+            ".jupyter_notebook_config.py"
+        )
+
+        self.lab_url = "http://localhost:{}".format(self.lab_port)
+
+    def launch_jupyterlab(self):
+        args = [self.lab_python_path, "-m", "jupyterlab",
+                "--port", self.lab_port,
+                "--config", self.notebook_config_path]
+        env = os.environ.copy()
+        env["HOME"] = self.lab_cwd
+        stdout = open(self.lab_log_path, 'ab')
+        proc = subprocess.Popen(
+            args,
+            cwd=self.lab_cwd,
+            env=env,
+            shell=False,
+            stdout=stdout,
+            stderr=subprocess.STDOUT
+        )
+        stdout.close()
+        self.lab_proc = proc
+        return proc
+
+    def run(self):
+        self.launch_jupyterlab()
+        __import__("time").sleep(10)
+        return webview.create_window("JupyterLab", self.lab_url)
+
+
+if __name__ == "__main__":
+    app = ViewApp()
+    sys.exit(app.run())
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 JupyterLabView
 ==============
 
+![JupyterLab](./jupyterlab.ico)
+
 基于 [Pywebview](https://github.com/r0x0r/pywebview) 将 [JupyterLab](https://github.com/jupyterlab/jupyterlab) 打包成桌面应用。
